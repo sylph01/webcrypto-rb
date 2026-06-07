151bd1db32fdbb6393339d1ed76c081d609a905d
Ryo Kajiwara <sylph01@s01.ninja>
2026-06-07 17:34:15 +0900

    AES-GCM and ECDSA

--- webcrypto.rb
+++ webcrypto.rb
@@ -2,23 +2,110 @@ require 'js'
 
 module WebCrypto
   module Util
+    def self.js_obj(hash)
+      obj = JS.global[:Object].new
+      hash.each { |k, v| obj[k] = v }
+      obj
+    end
+
     module JSArray
+      # Accept either a TypedArray or an ArrayBuffer
+      def self.view(js_obj)
+        name = js_obj[:constructor][:name].to_s
+        name == "ArrayBuffer" ? JS.global[:Uint8Array].new(js_obj) : js_obj
+      end
+
       def self.to_a(js_array)
-        js_array[:length].to_i.times.map { |i| js_array[i].to_i }
+        v = view(js_array)
+        v[:length].to_i.times.map { |i| v[i].to_i }
       end
 
-      def self.to_hex_a(js_array)
-        js_array[:length].to_i.times.map { |i| js_array[i].to_i.to_s(16).rjust(2, '0') }
+      def self.to_hex_array(js_array)
+        to_a(js_array).map { |b| b.to_s(16).rjust(2, '0') }
       end
 
-      def self.to_s(js_array)
-        self.to_a(js_array).pack('C*')
+      def self.to_bytes(js_array)
+        to_a(js_array).pack('C*')
       end
     end
   end
 
+  module CryptoKey
+    module Base
+      def algorithm_name
+        self[:algorithm][:name].to_s
+      end
+
+      def usages
+        u = self[:usages]
+        u[:length].to_i.times.map { |i| u[i].to_s }
+      end
+    end
+
+    module AESGCM
+      module Encrypt
+        def encrypt(plaintext, iv:)
+          encoded = plaintext.is_a?(String) ? JS.global[:TextEncoder].new.encode(plaintext) : plaintext
+          JS.global[:crypto][:subtle]
+            .encrypt(WebCrypto::Util.js_obj(name: "AES-GCM", iv: iv), self, encoded)
+            .await
+        end
+      end
+
+      module Decrypt
+        def decrypt(ciphertext, iv:)
+          JS.global[:crypto][:subtle]
+            .decrypt(WebCrypto::Util.js_obj(name: "AES-GCM", iv: iv), self, ciphertext)
+            .await
+        end
+      end
+    end
+
+    module ECDSA
+      module Sign
+        def sign(data, hash: "SHA-256")
+          JS.global[:crypto][:subtle]
+            .sign(WebCrypto::Util.js_obj(name: "ECDSA", hash: hash), self, data)
+            .await
+        end
+      end
+
+      module Verify
+        def verify(signature, data, hash: "SHA-256")
+          JS.global[:crypto][:subtle]
+            .verify(WebCrypto::Util.js_obj(name: "ECDSA", hash: hash), self, signature, data)
+            .await == JS::True
+        end
+      end
+    end
+
+    CAPABILITY_MAP = {
+      "AES-GCM" => { "encrypt" => AESGCM::Encrypt, "decrypt" => AESGCM::Decrypt },
+      "ECDSA"   => { "sign"    => ECDSA::Sign,     "verify"  => ECDSA::Verify   },
+      # extend as needed
+    }.freeze
+
+    def self.wrap(key)
+      sclass = (class << key; self; end)
+      sclass.include(Base)
+      mods = CAPABILITY_MAP[key.algorithm_name] || {}
+      key.usages.each do |u|
+        sclass.include(mods[u]) if mods[u]
+      end
+      key
+    end
+
+    def self.generate_key(algorithm, is_extractable, key_usages)
+      self.wrap(
+        JS.global[:crypto][:subtle]
+          .generateKey(WebCrypto::Util.js_obj(algorithm), is_extractable, key_usages)
+          .await
+      )
+    end
+  end
+
   def self.getRandomValues(length)
-    buf = JS.global[:Uint8Array].new(16)
+    buf = JS.global[:Uint8Array].new(length)
     JS.global[:crypto].getRandomValues(buf)
     buf
   end
